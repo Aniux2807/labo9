a4d295f618d1ba3a3a6e3b8739ddb6e390f0b1be
Aniux2807 <79299383+Aniux2807@users.noreply.github.com>
2021-04-12 12:53:26 -0600

    Add files via upload

---
+++ vissta.ascx.designer.cs
@@ -0,0 +1,17 @@
+//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Exercise_of_inheritance
+{
+
+
+    public partial class ViewSwitcher
+    {
+    }
+}
